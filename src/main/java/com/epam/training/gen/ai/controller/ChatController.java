af0cd72e36d5521c7f643576528c3444fda292b6
benghwee <swiftx888@gmail.com>
2025-03-15 11:14:39 +0000

    module 2 prompt engineering solution

---
+++ src/main/java/com/epam/training/gen/ai/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.epam.training.gen.ai.controller;
+
+
+import com.epam.training.gen.ai.service.HistoryPromptService;
+import com.epam.training.gen.ai.service.PromptService;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunction;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunctionArguments;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@Slf4j
+public class ChatController {
+    final PromptService promptService;
+
+    public ChatController(PromptService promptService, HistoryPromptService historyPromptService, Kernel kernel) {
+        this.promptService = promptService;
+    }
+
+    @GetMapping("/chat")
+    public List<String> chat(@RequestParam("input") String input){
+        return promptService.getChatCompletions(input).stream().toList();
+    }
+}
+
